352d9527fa41ed18e0d829716855d571f53dc6f2
yenifersan <yenifer.santiago@tecsup.edu.pe>
2019-06-25 13:40:22 -0500

    first_proye

---
+++ textimax-api/src/main/java/com/santiago/textimaxapi/TextimaxApiApplication.java
@@ -0,0 +1,13 @@
+package com.santiago.textimaxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextimaxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextimaxApiApplication.class, args);
+	}
+
+}
