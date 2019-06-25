cd8a4e8be0d612dc6c575ced850c6bdaeb06f2ab
yenifersan <yenifer.santiago@tecsup.edu.pe>
2019-06-25 13:30:24 -0500

    first_comit.

---
+++ STSGithubProject/src/main/java/com/santiago/www/STSGithubProject/App.java
@@ -0,0 +1,14 @@
+package com.santiago.www.STSGithubProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    
+    {
+        System.out.println( "Hello World!" );
+    }
+}
